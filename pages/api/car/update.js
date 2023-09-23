721cc26875bf581d6724060b1fd1549d5d175f0f
Matt <symplymatt@gmail.com>
2023-09-23 09:21:27 +0100

    fix

--- pages/api/car/update.js
+++ pages/api/car/update.js
@@ -50,8 +50,8 @@ export default async (req, res) => {
             const data = JSON.parse(body)
             // const images = JSON.parse(data.Images)
             const images = data.Images;
-            const base64Images = images.filter(i => !i.include('base64'));
-            const nonBase64Images = images.filter(i => i.include('base64'));
+            const base64Images = images.filter(i => !i.includes('base64'));
+            const nonBase64Images = images.filter(i => i.includes('base64'));
             // Base64-encoded image data
             const uploadImages = async() =>{
                 let imageUrls = [];
