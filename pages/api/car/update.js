881c2ff922095066ad7672007767b36802b799af
Matt <symplymatt@gmail.com>
2023-09-23 09:14:22 +0100

    fix

--- pages/api/car/update.js
+++ pages/api/car/update.js
@@ -9,6 +9,7 @@ import { v4 as uuidv4 } from 'uuid'
 import axios from "axios"
 import { EXTERNAL_API } from "../../../components/Constants"
 const prisma = new PrismaClient({})
+const cloudinary = require('cloudinary').v2;
 
 export const config = {
     api: {
@@ -31,6 +32,15 @@ function saveFile(file) {
     return fileurl
 }
 
+// Configure Cloudinary with your credentials
+cloudinary.config({
+    cloud_name: 'damltcdhl',
+    api_key: '289714989896362',
+    api_secret: 'wiqdgXyqTszEnWI_0POx6jM9P-0',
+  });
+  
+  
+
 export default async (req, res) => {
     const session = await getSession({ req })
     if (session) {
@@ -39,7 +49,31 @@ export default async (req, res) => {
         if (method == 'PUT') {
             const data = JSON.parse(body)
             // const images = JSON.parse(data.Images)
-            const images = data.Images
+            const images = data.Images;
+            const base64Images = images.filter(i => !i.include('base64'));
+            const nonBase64Images = images.filter(i => i.include('base64'));
+            // Base64-encoded image data
+            const uploadImages = async() =>{
+                let imageUrls = [];
+                const uploadResults = await base64Images.map(async image =>{
+                    // Upload the image
+                    try {
+                        const res = await cloudinary.uploader.upload(image, { resource_type: 'image' }, async (error, result) => {
+                            imageUrls.push(result.secure_url)
+                        if (error) {
+                            console.error('Error uploading image:', error);
+                        } else {
+                            console.log('Image uploaded:', result.secure_url);
+                        }
+                        });
+                    } catch (error) {
+                        
+                    }
+                });
+                const awaitPromises = await Promise.all(uploadResults);
+                return imageUrls
+
+            }
             // const iPrep = images.map(image => {
             //     if (image.match('base64')) {
             //         return saveFile(image)
@@ -62,7 +96,7 @@ export default async (req, res) => {
                 Week: parseInt(data.Week),
                 Month: parseInt(data.Month),
                 // Images: JSON.stringify(iPrep)
-                Images: images
+                Images: [...await uploadImages(), ...nonBase64Images]
             }
             const response = await axios.post(EXTERNAL_API + '/update', dataUpdate, {
                 headers: {
